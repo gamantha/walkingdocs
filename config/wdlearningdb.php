dd216f30b211962105a0bdaebfd7f2fce19390b6
root <root@ip-172-31-25-207.ap-southeast-1.compute.internal>
2020-01-29 10:59:25 +0000

    add gitignore for db

--- config/wdlearningdb.php
+++ config/wdlearningdb.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=walkingdocs_learning',
     'username' => 'root',
-    'password' => '',
+    'password' => '#root123',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
