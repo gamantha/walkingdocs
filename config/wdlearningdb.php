5fd6d89fee59ced74c138014948a5d3d16ee59aa
renowijoyo <renowijoyo@gmail.com>
2020-02-02 15:21:21 +0700

    ignore database config

--- config/wdlearningdb.php
+++ config/wdlearningdb.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=walkingdocs_learning',
     'username' => 'root',
-    'password' => '#root123',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
