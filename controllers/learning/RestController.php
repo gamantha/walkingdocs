04ca638eb17acfe6f8066f7f286ad35f483648f3
renowijoyo <renowijoyo@gmail.com>
2020-04-15 08:40:47 +0700

    add getquizall

--- controllers/learning/RestController.php
+++ controllers/learning/RestController.php
@@ -323,6 +323,88 @@ class RestController extends \yii\web\Controller
         // return $this->render('diagram');
     }
 
+    public function actionGetquizall()
+    {
+        $appPath = Yii::getAlias('@app');
+
+        $files = scandir($appPath . '/assets/checklists', 1);
+
+        $json_content = file_get_contents($appPath . '/assets/checklists/' . $files[0]);
+
+        $ret = [];
+        function is_json($string,$return_data = false) {
+            $data = json_decode($string);
+            return (json_last_error() == JSON_ERROR_NONE) ? ($return_data ? $data : TRUE) : FALSE;
+        }
+
+        $json_object = json_decode($json_content);
+
+//        $result = (new JSONPath($json_object))->find('$..reference'); // returns new JSONPath
+        $diff_results = (new JSONPath($json_object))->find('$..[?(@.differential_diagnosis)]'); // returns new JSONPath
+        $bg_results = (new JSONPath($json_object))->find('$..[?(@.background)]'); // returns new JSONPath
+        foreach($diff_results as $diff_result) {
+            $temparray = [];
+//            echo "what is the differential diagnosis for " . $diff_result->name->text . '<br/>';
+            if (sizeof($diff_result->differential_diagnosis) > 0) {
+                $tempstring = '';
+                foreach($diff_result->differential_diagnosis as $diff_diag) {
+                    $tempstring = $tempstring . json_encode($diff_diag->name->text) . ', ';
+                }
+                $temparray['preface'] = 'What is the ';
+                $temparray['type'] = 'differential_diagnosis';
+                $temparray['question'] = $tempstring;
+                if (strpos($diff_result->name->text, '(')) {
+                    $temparray['answer'] = substr($diff_result->name->text, 0, strpos($diff_result->name->text, '(')) ;
+                } else {
+                    $temparray['answer'] = $diff_result->name->text;
+                }
+
+
+                array_push($ret, $temparray);
+            }
+        }
+
+        foreach($bg_results as $bg_result) {
+            $temparray = [];
+//            echo "what is the differential diagnosis for " . $diff_result->name->text . '<br/>';
+            if (($bg_result->background->text == null)
+                || ($bg_result->name->text == null)
+                || ($bg_result->name->text == "")
+                || ($bg_result->background->text == "")
+            ){
+
+            } else {
+
+
+                $tempstring = $bg_result->background->text;
+
+                $temparray['preface'] = 'Describe ';
+                $temparray['type'] = 'background';
+                if(strpos($bg_result->name->text, '(')) {
+                    $temparray['question'] = substr($bg_result->name->text, 0, strpos($bg_result->name->text, '(')) ;
+                } else {
+                    $temparray['question'] = $bg_result->name->text;
+                }
+
+
+                $temparray['answer'] = $tempstring;
+
+//                echo '<hr/>';
+                array_push($ret, $temparray);
+            }
+        }
+
+
+//        $randoms = array_rand($ret,10);
+        $randoms = $ret;
+
+
+
+        \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        return $randoms;
+//        sizeof($randoms);
+
+    }
 
     public function actionGetquiz()
     {
