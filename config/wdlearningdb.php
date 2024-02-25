450565e32eef781d5f3b096aa23023204f2ef510
Reno Wijoyo <renowijoyo@gmail.com>
2024-02-25 08:51:54 +0700

    update db

--- config/wdlearningdb.php
+++ config/wdlearningdb.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=walkingdocs_learning',
-    'username' => 'wduser',
-    'password' => 'walkingdocs2021',
+    'username' => 'root',
+    'password' => 'ROOT123!',
     'charset' => 'utf8',
     'attributes' => [PDO::ATTR_CASE => PDO::CASE_LOWER],
 
